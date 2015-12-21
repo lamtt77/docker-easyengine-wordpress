3db736b0b26ec554bbda90d8111c4412a966a6d8
CoreOS Admin <core@gcore3.c.lamdocker.internal>
2015-12-21 13:52:32 +0000

    changed ownership for dirs of mysql and www-data

--- ee-stack/startStack.sh
+++ ee-stack/startStack.sh
@@ -2,4 +2,5 @@
 
 echo "Services (MariaDB, Nginx, PHP, etc) are starting, please wait..."
 ee stack start
+chown -R mysql:mysql /var/lib/mysql && chown -R www-data:www-data /var/www
 tail -f /var/log/ee/ee.log
