ef2065ccc7a36684db97fcfeb07d88eba50da682
CoreOS Admin <core@gcore2.c.lamdocker.internal>
2015-12-22 16:40:01 +0000

    env variables supported for web and db

--- ee-db/grantRemoteAccess.sh
+++ ee-db/grantRemoteAccess.sh
@@ -1,4 +1,2 @@
 chown -R mysql:mysql /var/lib/mysql
-
-grant all privileges on *.* to 'root'@'%'  IDENTIFIED BY 'MYSQL_ROOT_PASSWORD' with grant option;
-flush privileges;
+mysql -e "grant all privileges on *.* to 'root'@'%' with grant option; flush privileges;"
--- ee-web/startWeb.sh
+++ ee-web/startWeb.sh
@@ -6,6 +6,7 @@ echo "Services (Nginx, Php5-fpm, etc) are starting, please wait..."
 sed -i "s/REMOTE_MYSQL_HOST/$REMOTE_MYSQL_HOST/" /etc/mysql/conf.d/my.cnf
 sed -i "s/REMOTE_MYSQL_USER/$REMOTE_MYSQL_USER/" /etc/mysql/conf.d/my.cnf
 sed -i "s/REMOTE_MYSQL_PASSWORD/$REMOTE_MYSQL_PASSWORD/" /etc/mysql/conf.d/my.cnf
+sed -i "s/PUBLIC_IP_OF_EASYENGINE_SERVER/$PUBLIC_IP_OF_EASYENGINE_SERVER/" /etc/ee/ee.conf
 
 ee stack start
 tail -f /var/log/ee/ee.log
