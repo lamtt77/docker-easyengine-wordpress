1fdbbeb094601e16da12f0b88a81fd3a971a79dc
CoreOS Admin <core@gcore2.c.lamdocker.internal>
2015-12-22 08:16:51 +0000

    remote db access 2

--- ee-db/startDB.sh
+++ ee-db/startDB.sh
@@ -2,6 +2,4 @@
 
 echo "MariaDB is Starting, please wait..."
 ee stack start --mysql
-chown -R mysql:mysql /var/lib/mysql
-mysql -u root -p 'OACPbwFy' < grantRemoteAccess.sql
 tail -f /var/log/ee/ee.log
