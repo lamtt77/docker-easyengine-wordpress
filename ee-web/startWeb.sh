228d076e41fb6e1c5599602167a328b79dd4c15a
CoreOS Admin <core@gcore2.c.lamdocker.internal>
2015-12-22 14:23:21 +0000

    env variables added for web also

--- ee-web/startWeb.sh
+++ ee-web/startWeb.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "Services (Nginx, Php5-fpm, etc) are starting, please wait..."
+
+# A bit hacky to support dynamic env variables as Docker yet to support -- just to hide psw in the auto build
+sed -i "s/REMOTE_MYSQL_HOST/$REMOTE_MYSQL_HOST/" /etc/mysql/conf.d/my.cnf
+sed -i "s/REMOTE_MYSQL_USER/$REMOTE_MYSQL_USER/" /etc/mysql/conf.d/my.cnf
+sed -i "s/REMOTE_MYSQL_PASSWORD/$REMOTE_MYSQL_PASSWORD/" /etc/mysql/conf.d/my.cnf
+
 ee stack start
 tail -f /var/log/ee/ee.log
