70e0167783313c21212bf5b9e6e34915b61efcbe
Lam Tran <lamtt77@gmail.com>
2015-12-24 23:56:04 +0700

    enabled data volumes mapping and prepared for haproxy

--- ee-db/grantRemoteAccess.sh
+++
@@ -1,3 +0,0 @@
-chown -R mysql:mysql /var/lib/mysql
-mysql -e "grant all privileges on *.* to 'root'@'$PUBLIC_IP_OF_EASYENGINE_SERVER' \
-  IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' with grant option; flush privileges;"
--- ee-db/startDB.sh
+++ ee-db/startDB.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "MariaDB is starting, please wait..."
-ee stack start --mysql
 
-./grantRemoteAccess.sh
+chown -R mysql:mysql /var/lib/mysql
+ee stack start --mysql
+mysql -e "grant all privileges on *.* to 'root'@'$PUBLIC_IP_OF_EASYENGINE_SERVER' \
+  IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' with grant option; flush privileges;"
 
 tail -f /var/log/ee/ee.log
