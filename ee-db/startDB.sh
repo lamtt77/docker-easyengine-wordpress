3c33815407a4c86d4875abdccb65ea2084f01736
Lam Tran <lamtt77@gmail.com>
2015-12-25 01:04:37 +0700

    added host directory volume map and data container map

--- ee-db/startDB.sh
+++ ee-db/startDB.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "===============================================>"
 echo "MariaDB is starting, please wait..."
 
+chown -R mysql:mysql /var/lib/mysql
 ee stack start --mysql
 mysql -e "grant all privileges on *.* to 'root'@'$PUBLIC_IP_OF_EASYENGINE_SERVER' \
   IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' with grant option; flush privileges;"
--- ee-web/startWeb.sh
+++ ee-web/startWeb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "===============================================>"
 echo "Services (Nginx, Php5-fpm, etc) are starting, please wait..."
 
 # A bit hacky to enable dynamic env variables as Docker yet to support -- just to hide psw in the auto build
@@ -8,6 +9,7 @@ sed -i "s/REMOTE_MYSQL_USER/$REMOTE_MYSQL_USER/" /etc/mysql/conf.d/my.cnf
 sed -i "s/REMOTE_MYSQL_PASSWORD/$REMOTE_MYSQL_PASSWORD/" /etc/mysql/conf.d/my.cnf
 sed -i "s/PUBLIC_IP_OF_EASYENGINE_SERVER/$PUBLIC_IP_OF_EASYENGINE_SERVER/" /etc/ee/ee.conf
 
+chown -R www-data:www-data /var/www
 ee stack start
 
 echo '====>Configuration finished! Now reporting latest status of all services...'
