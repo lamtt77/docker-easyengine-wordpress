ac7888677360a42f1aa9e9efd4e3b3b33c5b377d
Lam Tran <lamtt77@gmail.com>
2015-12-27 20:23:11 +0700

    added Default Services start-up

---
+++ scripts/setupDefaultDockerServies.sh
@@ -0,0 +1,6 @@
+update-rc.d -f mysql remove
+update-rc.d -f nginx remove
+update-rc.d -f php5-fpm remove
+update-rc.d -f postfix remove
+
+update-rc.d -f redis-server remove
---
+++ scripts/setupDefaultHostServices.sh
@@ -0,0 +1,6 @@
+update-rc.d mysql defaults
+update-rc.d nginx defaults
+update-rc.d php5-fpm defaults
+update-rc.d postfix defaults
+
+update-rc.d redis-server defaults
