9b9ac0d3da35b5cac6e28eb632f0871486126eda
Lam Tran <lamtt77@gmail.com>
2015-12-29 22:15:15 +0700

    minor tweak

--- ee-db/startDB.sh
+++ ee-db/startDB.sh
@@ -4,6 +4,8 @@ echo "===============================================>"
 echo "MariaDB is starting, please wait..."
 
 chown -R mysql:mysql /var/lib/mysql
+sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/" /etc/mysql/conf.d/my.cnf
+
 ee stack start --mysql
 
 mysql -e "grant all privileges on *.* to 'root'@'$PUBLIC_IP_OF_EASYENGINE_SERVER' \
